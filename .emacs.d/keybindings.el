b386bd0f302701a1047f8ebd08b56ff5fb311749
nukeop <nukeop@gumblert.tech>
2018-05-27 00:29:40 +0200

    Add bindingsfor eyebrowse

--- .emacs.d/keybindings.el
+++ .emacs.d/keybindings.el
@@ -17,7 +17,21 @@
 (global-set-key (kbd "M-<right>") 'windmove-right)
 (global-set-key (kbd "M-<up>") 'windmove-up)
 (global-set-key (kbd "M-<down>") 'windmove-down)
+(global-set-key (kbd "s-1") 'eyebrowse-switch-to-window-config-1)
+(global-set-key (kbd "s-2") 'eyebrowse-switch-to-window-config-2)
+(global-set-key (kbd "s-3") 'eyebrowse-switch-to-window-config-3)
+(global-set-key (kbd "s-4") 'eyebrowse-switch-to-window-config-4)
+(global-set-key (kbd "s-5") 'eyebrowse-switch-to-window-config-5)
+(global-set-key (kbd "s-6") 'eyebrowse-switch-to-window-config-6)
+(global-set-key (kbd "s-7") 'eyebrowse-switch-to-window-config-7)
+(global-set-key (kbd "s-8") 'eyebrowse-switch-to-window-config-8)
+(global-set-key (kbd "s-9") 'eyebrowse-switch-to-window-config-9)
+(global-set-key (kbd "s-0") 'eyebrowse-switch-to-window-config-0)
+(global-set-key (kbd "s-a") 'eyebrowse-prev-window-config)
+(global-set-key (kbd "s-d") 'eyebrowse-next-window-config)
+
 (global-set-key [f9] 'eval-buffer)
+
 
 ;; Custom functions
 (global-set-key (kbd "C-S-d") 'duplicate-line)
