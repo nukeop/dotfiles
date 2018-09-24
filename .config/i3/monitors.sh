a5f189b6765433110bc588a105ba5a838646cd62
nukeop <nukeop@gumblert.tech>
2018-09-24 23:22:19 +0200

    update i3 and compton config

--- .config/i3/monitors.sh
+++ .config/i3/monitors.sh
@@ -1,1 +1,2 @@
-xrandr | grep -q "DP2-3" && xrandr --output eDP1 --auto --output DP2-3 --auto --left-of eDP1 --output DP2-2 --off & # --output DP2-2 --auto --rotate left --right-of eDP1
+#!/bin/bash
+xrandr | grep -q "DP2-3" && xrandr --output eDP1 --auto --output DP2-3 --auto --left-of eDP1 #--output DP2-2 --auto --rotate left --right-of eDP1
