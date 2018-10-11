557c940ec5fe79b5f7ad0d0caf91e114d74e1fb5
nukeop <nukeop@gumblert.tech>
2018-10-11 10:05:04 +0200

    update scripts

--- .config/i3/monitors.sh
+++ .config/i3/monitors.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-xrandr | grep -q "DP2-3" && xrandr --output eDP1 --auto --output DP2-3 --auto --left-of eDP1 #--output DP2-2 --auto --rotate left --right-of eDP1
+xrandr | grep -q "DP-2-3" && xrandr --output eDP-1 --auto --output DP-2-3 --auto --left-of eDP-1 --output DP2-2 --auto --rotate left --right-of eDP-1
