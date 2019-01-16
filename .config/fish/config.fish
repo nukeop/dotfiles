8692c9444c89ac387232561b3ca9bc64d5b41fe8
nukeop <nukeop@gumblert.tech>
2019-01-16 11:00:17 +0100

    Add fish config

---
+++ .config/fish/config.fish
@@ -0,0 +1,16 @@
+alias fx="firefox --new-instance --profile (mktemp -d)"
+
+alias g="git"
+alias ga="git add"
+alias gc="git clone"
+alias gcm="git commit -m"
+alias gs="git status"
+
+alias ni="npm install"
+
+
+function .. ; cd .. ; end
+
+function cheat
+  curl cht.sh/$argv
+end
