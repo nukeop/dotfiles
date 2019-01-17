371b5c88b5ebb11b8c06c15d1153433822e01d3a
nukeop <nukeop@gumblert.tech>
2019-01-17 10:20:38 +0100

    Add battery script to polyb

---
+++ .config/polybar/battery.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+capacity1=$(cat /sys/class/power_supply/BAT0/capacity)
+capacity2=$(cat /sys/class/power_supply/BAT1/capacity)
+bar=$(spark 0 $capacity1 $capacity2 100)
+graph1=${bar:1:1}
+graph2=${bar:2:1}
+echo $graph1 $capacity1"%" $graph2 $capacity2"%"
