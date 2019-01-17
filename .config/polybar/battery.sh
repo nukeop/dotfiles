fe24709e5455e6c5a2095f37f3b7e929a08220fc
nukeop <nukeop@gumblert.tech>
2019-01-17 10:54:44 +0100

    update battery script

--- .config/polybar/battery.sh
+++ .config/polybar/battery.sh
@@ -4,4 +4,4 @@ capacity2=$(cat /sys/class/power_supply/BAT1/capacity)
 bar=$(spark 0 $capacity1 $capacity2 100)
 graph1=${bar:1:1}
 graph2=${bar:2:1}
-echo $graph1 $capacity1"%" $graph2 $capacity2"%"
+echo $graph1 "["$capacity1"]%" $graph2 "["$capacity2"]%"
