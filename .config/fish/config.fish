8bba32f0aa3298406d21d35a34b158cb22906526
nukeop <nukeop@gumblert.tech>
2019-10-05 20:20:27 +0200

    Add git checkout alias

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,11 +3,11 @@ alias fx="firefox --new-instance --profile (mktemp -d)"
 alias g="git"
 alias ga="git add"
 alias gc="git clone"
+alias gch="git checkout"
 alias gcm="git commit -m"
 alias gs="git status"
 
 alias ni="npm install"
-
 
 function .. ; cd .. ; end
 
