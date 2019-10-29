9887822b68716f3122a058e9a6baf7c77cabff0d
nukeop <nukeop@gumblert.tech>
2019-10-29 02:03:59 +0100

    Update fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,13 +1,22 @@
 alias fx="firefox --new-instance --profile (mktemp -d)"
 
+alias tmux="env TERM=xterm-256color tmux"
+alias enw="env TERM=xterm-256color emacs -nw"
+
 alias g="git"
 alias ga="git add"
 alias gc="git clone"
-alias gch="git checkout"
 alias gcm="git commit -m"
+alias gch="git checkout"
 alias gs="git status"
 
+alias dcu="docker-compose up"
+alias ls="exa --icons"
+alias cat="bat"
+
 alias ni="npm install"
+
+source ~/.cargo/env
 
 function .. ; cd .. ; end
 
